469c18716c569c395795a051dfbc0abf753d12eb
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-06-06 13:17:31 +0200

    Angular animations ed esercizio CSS
    Lezione del 06/06/2023, esercizio fatto in css puro da simone

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showHide', [
+      state('show', style({
+        left: '500px',
+        opacity: 1
+      })),
+      state('hide', style({
+        left: '-300px',
+        opacity: 0
+      })),
+      transition('show => hide', [
+        animate('1s')
+      ]),
+      transition('hide => show', [
+        animate('1s')
+      ])
+    ])
+  ]
+})
+export class AppComponent {
+  title = 'angular-animations';
+  isShow = true;
+
+  showHide(){
+    this.isShow = !this.isShow;
+    // document.getElementById('simo')?.classList.toggle('animate');
+    if (this.isShow) {
+      document.getElementById('simo')?.classList.add('hide');
+      document.getElementById('simo')?.classList.remove('show');
+    } else{
+      document.getElementById('simo')?.classList.add('show');
+      document.getElementById('simo')?.classList.remove('hide');
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OpenCloseComponent } from './components/open-close/open-close.component';
+import { SimoAnimationsComponent } from './simo-animations/simo-animations.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OpenCloseComponent,
+    SimoAnimationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
